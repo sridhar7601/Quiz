7dc24ca8b6b40df5c84a2c5db427f8f4d66f93b5
srisru <sridzar1234@gmail.com>
2023-05-19 16:37:35 +0530

    new change

--- App.js
+++ App.js
@@ -1,93 +1,73 @@
-import React, { useState, useEffect } from 'react';
-import { Text, View, ScrollView } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
-import axios from 'axios';
 
-const styles = {
-  contentContainer: {
-    padding: 20,
-    marginBottom: 20,
-    borderRadius: 10,
-    shadowColor: '#000',
-    shadowOffset: {
-      width: 0,
-      height: 1,
-    },
-    shadowOpacity: 0.22,
-    shadowRadius: 2.22,
-    elevation: 3,
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <LinearGradient
+        colors={['#f731db', '#4600f1']}
+        start={{ x: 0, y: 1 }}
+        end={{ x: 1, y: 1 }}
+        style={styles.card}
+      >
+        <Text style={styles.heading}>Heading</Text>
+        <Text style={styles.subheading}>Subheading 1</Text>
+        <Text style={styles.subheading}>Subheading 2</Text>
+        <Text style={styles.subheading}>Subheading 3</Text>
+        <Text style={styles.subheading}>Subheading 4</Text>
+        <Text style={styles.subheading}>Subheading 5</Text>
+      </LinearGradient>
+      {/* <TouchableOpacity style={styles.button}>
+      </TouchableOpacity> */}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#fff',
   },
   card: {
-    minHeight: 100,
-    borderRadius: 10,
+    width: 300,
     padding: 20,
+    borderRadius: 25,
+    shadowColor: '#ccc',
+    shadowOffset: {
+      width: 5,
+      height: 5,
+    },
+    shadowOpacity: 1,
+    shadowRadius: 10,
+    elevation: 5,
     marginBottom: 20,
   },
   heading: {
     fontSize: 24,
     fontWeight: 'bold',
-    color: '#fff',
     marginBottom: 10,
+    color: '#fff',
   },
   subheading: {
-    fontSize: 18,
-    color: '#fff',
-    marginBottom: 5,
-  },
-  scrollContent: {
-    marginTop: 10,
-    borderTopWidth: 1,
-    borderTopColor: '#ccc',
-    paddingTop: 10,
-  },
-  scrollText: {
     fontSize: 16,
-    marginBottom: 5,
+    color: '#fff',
+    lineHeight: 25,
   },
-};
-
-const Cards = () => {
-  const [cards, setCards] = useState([]);
-
-  useEffect(() => {
-    // Fetch data from API
-    axios
-      .get('http://localhost:8080/publish/getp/1')
-      .then((response) => {
-        setCards(response.data);
-      })
-      .catch((error) => {
-        console.error(error);
-      });
-  }, []);
-
-  return (
-    <ScrollView>
-      {cards.length === 0 ? (
-        <Text>Loading...</Text> // or display a loading spinner
-      ) : (
-        cards.map((card, index) => (
-          <View key={index} style={styles.contentContainer}>
-            <LinearGradient
-              colors={['#f731db', '#4600f1']}
-              start={{ x: 0, y: 1 }}
-              end={{ x: 1, y: 1 }}
-              style={styles.card}>
-              <Text style={styles.heading}>{card.title}</Text>
-              <Text style={styles.subheading}>{card.questions}</Text>
-            </LinearGradient>
-            {/* <View style={styles.scrollContent}>
-              {Array.from({ length: 50 }).map((_, index) => (
-                <Text key={index} style={styles.scrollText}>
-                  Scrollable Content {index + 1}
-                </Text>
-              ))}
-            </View> */}
-          </View>
-        ))
-      )}
-    </ScrollView>
-  );
-};
-
-export default Cards;
+  button: {
+    backgroundColor: '#fff',
+    borderRadius: 50,
+    width: 150,
+    height: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 40,
+  },
+  buttonText: {
+    fontWeight: 'bold',
+    textTransform: 'uppercase',
+    color: '#4600f1',
+  },
+});
--- babel.config.js
+++ babel.config.js
@@ -4,3 +4,6 @@ module.exports = function(api) {
     presets: ['babel-preset-expo'],
   };
 };
+
+
+
